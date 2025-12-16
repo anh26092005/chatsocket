204adc6562a53555724f70d3c8eec84fb41de7f4
Anh <anhdlcwk@gmail.com>
2025-12-16 23:28:06 +0700

    khai bao schema

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,21 @@
 export const signup = async (req, res) => {
-    res.send("Signup Endpoint");
+
+    const { fullName, emal, password } = req.body;
+    try {
+        if (!fullName || !emal || !password) {
+            return res.status(400).json({ message: " All fields are required" })
+        }
+        if (password.length < 6) {
+            return res.status(400).json({ message: "Password must be at least 6 characters long" });
+        }
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ message: "Invalid email format" });
+        }
+
+    }
+    catch (error) {
+
+    }
 
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,7 @@ import { connectDB } from "./lib/db.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+app.use(express.json());  //req.body
 const __dirname = path.resolve();
 // Routes
 app.use("/api/auth", authRoutes);
