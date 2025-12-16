9fa0c55cc01abf8d760f7a24754f183a6ff0911e
Anh <anhdlcwk@gmail.com>
2025-12-16 22:05:51 +0700

    a

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -2,5 +2,8 @@ import express from "express";
 const router = express.Router();
 router.get("/send", (req, res) => {
     res.send("Message endpoint");
-})
+});
+router.get("/get", (req, res) => {
+    res.send("Message endpoint");
+});
 export default router;
